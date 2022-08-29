0dd4ea228ff1b29c3f51da7a26244c9a03eba345
Kere032 <88923295+Kere032@users.noreply.github.com>
2022-08-29 11:24:56 -0400

    Update http.php

--- http.php
+++ http.php
@@ -6,46 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style1.css"/>
     <title>Free Anonymous Proxies</title>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-RBSG15RNLW"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-
-    gtag('config', 'G-RBSG15RNLW');
-  </script>
 </head>
 <body>
-    <nav>
-        <div class="hamburger">
-            <div class="line1"></div>
-            <div class="line2"></div>
-            <div class="line3"></div>
-        </div>
-        <ul class="nav-links">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="http.php">http</a></li>
-            <li><a href="socks4.php">Socks4</a></li>
-            <li><a href="socks5.php">Socks5</a></li>
-        </ul>
-    </nav>
-
-    <div class="container">
-<br><br><br><br><br><br><br><br><br><br><br><br>
-    
-      <h1 class="display-4">http/https</h1>
-
       <?php
                 $text = file_get_contents('http://proxy-root.me/http/?key=5e361fdfcde0b12fe29d394284338fd55a99b475 ');
                 echo $text;
             ?>
-
-  </section>
-
-
-  <script src="script.js"></script>
-
 </body>
 
 </html>
